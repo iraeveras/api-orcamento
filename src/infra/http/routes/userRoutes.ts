cb989bae8fc0eabab8f372625925182ce930e07c
Irae Veras <irae.online@gmail.com>
2025-06-26 01:02:27 -0300

    refactor: inject routes reset password and middlewares validate and authenticate

--- src/infra/http/routes/userRoutes.ts
+++ src/infra/http/routes/userRoutes.ts
@@ -1,13 +1,21 @@
 // src/infra/http/routes/userRoutes.ts
 import { Router } from 'express';
-import { createUser, listUsers, updadeUser, deleteUser } from '@/infra/http/controllers/userController';
+import { createUser, listUsers, updateUser, deleteUser, forgotPassword, resetPassword, login } from '@/infra/http/controllers/userController';
+import { validate } from '@/infra/middlewares/validate';
+import { authenticate } from '@/infra/middlewares/authenticate';
+import { authorize } from '@/infra/middlewares/authorize';
+import { createUserSchema, loginSchema, forgotPasswordSchema, resetPasswordSchema } from '@/domain/validations/userSchemas';
 import { asyncHandler } from '@/shared/utils/asyncHandler';
 
 const router = Router();
 
-router.post('/', asyncHandler(createUser));
-router.get('/', asyncHandler(listUsers));
-router.put('/:id', asyncHandler(updadeUser));
-router.delete('/:id', asyncHandler(deleteUser));
+router.post('/login', validate(loginSchema), login);
+router.post('/forgot-password', validate(forgotPasswordSchema), forgotPassword);
+router.post('reset-password', validate(resetPasswordSchema), resetPassword)
+
+router.post('/', authenticate, authorize('users', ['write']), validate(createUserSchema), asyncHandler(createUser));
+router.get('/', authenticate, authorize('users', ['read']), asyncHandler(listUsers));
+router.put('/:id', authenticate, authorize('users', ['write']), asyncHandler(updateUser));
+router.delete('/:id', authenticate, authorize('users', ['delete']), asyncHandler(deleteUser));
 
 export default router;
