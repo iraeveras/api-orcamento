b51059a622a48c8bc00d6ed10d33bf6e7cdc5b26
Irae Veras <irae.online@gmail.com>
2025-06-25 21:59:14 -0300

    fix: imports user routes operations

--- src/infra/http/routes/userRoutes.ts
+++ src/infra/http/routes/userRoutes.ts
@@ -1,10 +1,13 @@
 // src/infra/http/routes/userRoutes.ts
 import { Router } from 'express';
-import { createUser } from '@/infra/http/controllers/userController';
+import { createUser, listUsers, updadeUser, deleteUser } from '@/infra/http/controllers/userController';
 import { asyncHandler } from '@/shared/utils/asyncHandler';
 
 const router = Router();
 
 router.post('/', asyncHandler(createUser));
+router.get('/', asyncHandler(listUsers));
+router.put('/:id', asyncHandler(updadeUser));
+router.delete('/:id', asyncHandler(deleteUser));
 
 export default router;
