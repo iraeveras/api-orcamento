45d5df6807435081f8839b57b3984a1cf8c7fbac
Irae Veras <irae.online@gmail.com>
2025-06-28 20:27:38 -0300

    fix: added asyncHandler

--- src/infra/http/routes/userRoutes.ts
+++ src/infra/http/routes/userRoutes.ts
@@ -1,4 +1,4 @@
-// src/infra/http/routes/userRoutes.ts
+// FILE: src/infra/http/routes/userRoutes.ts
 import { Router } from 'express';
 import { createUser, listUsers, updateUser, deleteUser, forgotPassword, resetPassword, login } from '@/infra/http/controllers/userController';
 import { validate } from '@/infra/middlewares/validate';
@@ -9,9 +9,9 @@ import { asyncHandler } from '@/shared/utils/asyncHandler';
 
 const router = Router();
 
-router.post('/login', validate(loginSchema), login);
-router.post('/forgot-password', validate(forgotPasswordSchema), forgotPassword);
-router.post('reset-password', validate(resetPasswordSchema), resetPassword)
+router.post('/login', validate(loginSchema), asyncHandler(login));
+router.post('/forgot-password', validate(forgotPasswordSchema), asyncHandler(forgotPassword));
+router.post('reset-password', validate(resetPasswordSchema), asyncHandler(resetPassword));
 
 router.post('/', authenticate, authorize('users', ['write']), validate(createUserSchema), asyncHandler(createUser));
 router.get('/', authenticate, authorize('users', ['read']), asyncHandler(listUsers));
